d91f79a9d55967131d62484004172576f2c31651
davidra <divad.nosnilwar@gmail.com>
2017-04-04 18:33:22 +1000

    ready to run GNG again

--- variables/variables-jenkins.sh
+++ variables/variables-jenkins.sh
@@ -19,8 +19,8 @@ export AGI_RUN_HOME=/root/dev/run
 #export AGI_EXP_HOME=$WORKSPACE/experiment-definitions/20171201-mnist-ksparse
 #export AGI_EXP_HOME=$WORKSPACE/experiment-definitions/20171201-mnist-ksparse
 #export AGI_EXP_HOME=$WORKSPACE/experiment-definitions/20170104-mnist-onlineksparse-ph1
-#export AGI_EXP_HOME=$WORKSPACE/experiment-definitions/20170104-mnist-quilted-gng-ph1
-export AGI_EXP_HOME=$WORKSPACE/experiment-definitions/20170404-mnist-onlineksparse-real
+export AGI_EXP_HOME=$WORKSPACE/experiment-definitions/20170104-mnist-quilted-gng-ph1
+#export AGI_EXP_HOME=$WORKSPACE/experiment-definitions/20170404-mnist-onlineksparse-real
 
 # DATA Home
 export AGI_DATA_HOME=$AGI_EXP_HOME		# there is no Data on Jenkins, but needs to be a real folder
