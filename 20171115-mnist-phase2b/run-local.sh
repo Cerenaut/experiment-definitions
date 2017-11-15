069e5b581716880f807972e2c7d26322d7c5de39
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-11-15 13:19:26 +1100

    Add Phase2b exp def

---
+++ 20171115-mnist-phase2b/run-local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source $VARIABLES_FILE
+
+prefixes=`cat prefixes.txt`
+
+cmd="python run-framework.py
+  --exps_file experiments-phase2.json
+  --step_compute
+  --step_export
+  --step_upload
+  --host localhost
+  --port 8491
+  --no_docker"
+
+./runexp.sh $AGI_EXP_HOME "${AGI_RUNFRAMEWORK_PATH}/scripts/run-framework" "$cmd" $prefixes # ${prefixes[*]}
+
+exit
